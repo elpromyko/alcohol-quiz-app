a8905524c47475eedfca1703f2b87b0d303c9259
Piotr Prominski <piotr.prominski@gmail.com>
2018-01-24 19:31:39 +0100

    improve logic

--- app/src/main/java/com/example/android/alcoholquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/alcoholquizapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.android.alcoholquizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +13,9 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     CheckBox answerVodka, answerGin, answerStirred, answerShaken, answerChile, answerBolivia,
             answerBrazil, answerPeru;
@@ -23,15 +25,21 @@ public class MainActivity extends AppCompatActivity {
             piscoAnswerButton, maryAnswerButton, wineAnswerButton;
     RadioGroup groupWhisky, groupRum, groupMary, groupWine;
     LinearLayout groupBond, groupPisco;
+    List<Button> answerButtons;
+    List<LinearLayout> groups;
+    List<RadioButton> radioCorrectAnswers;
+    TextView finalScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void submitAnswers(View view) {
+
+        finalScore = findViewById(R.id.final_score);
+
         // buttons
         submitButton = findViewById(R.id.submit_button);
         resetButton = findViewById(R.id.reset_button);
@@ -68,7 +76,12 @@ public class MainActivity extends AppCompatActivity {
         groupMary = findViewById(R.id.group_mary);
         groupWine = findViewById(R.id.group_wine);
 
-//        Log.v("MainActivity", "gin " + checkedGin);
+        answerButtons = Arrays.asList(bondAnswerButton, rumAnswerButton, ginAnswerButton,
+                whiskyAnswerButton, piscoAnswerButton, maryAnswerButton, wineAnswerButton);
+
+        groups = Arrays.asList(groupWhisky, groupRum, groupMary, groupWine, groupBond, groupPisco);
+
+        radioCorrectAnswers = Arrays.asList(answerSugarcane, answerAllCountries, answerTomato, answerVatican);
 
         changeButtonState(false);
 
@@ -83,25 +96,20 @@ public class MainActivity extends AppCompatActivity {
             if ((answerVodka.isChecked()) && (answerShaken.isChecked())) score++;
         }
 
-        if (answerSugarcane.isChecked()) score++;
-
-        if (userTypedAnswer.getText().toString().equalsIgnoreCase("gin")) score++;
-
-        if (answerAllCountries.isChecked()) score++;
-
         if ((! answerBolivia.isChecked()) && (! answerBrazil.isChecked())) {
             if ((answerChile.isChecked()) && (answerPeru.isChecked())) score++;
         }
 
-        if (answerTomato.isChecked()) score++;
+        if (userTypedAnswer.getText().toString().equalsIgnoreCase(getResources().getString(R.string.bond_answer_2))) score++;
 
-        if (answerVatican.isChecked()) score++;
+        for (RadioButton correctAnswer : radioCorrectAnswers) {
+            if (correctAnswer.isChecked()) score++;
+        }
 
         return score;
     }
 
     public void displayFinalScore(int score) {
-        TextView finalScore = findViewById(R.id.final_score);
         finalScore.setText("SCORE " + score);
     }
 
@@ -109,32 +117,16 @@ public class MainActivity extends AppCompatActivity {
         userTypedAnswer.setFocusable(state);
         userTypedAnswer.setEnabled(state);
         submitButton.setEnabled(state);
-        resetButton.setVisibility(View.VISIBLE);
-        bondAnswerButton.setVisibility(View.VISIBLE);
-        rumAnswerButton.setVisibility(View.VISIBLE);
-        ginAnswerButton.setVisibility(View.VISIBLE);
-        whiskyAnswerButton.setVisibility(View.VISIBLE);
-        piscoAnswerButton.setVisibility(View.VISIBLE);
-        maryAnswerButton.setVisibility(View.VISIBLE);
-        wineAnswerButton.setVisibility(View.VISIBLE);
 
-        for (int i = 0; i < groupWhisky.getChildCount(); i++) {
-            groupWhisky.getChildAt(i).setEnabled(state);
+        resetButton.setVisibility(View.VISIBLE);
+        for (Button btn : answerButtons) {
+            btn.setVisibility(View.VISIBLE);
         }
-        for (int i = 0; i < groupBond.getChildCount(); i++) {
-            groupBond.getChildAt(i).setEnabled(state);
-        }
-        for (int i = 0; i < groupPisco.getChildCount(); i++) {
-            groupPisco.getChildAt(i).setEnabled(state);
-        }
-        for (int i = 0; i < groupRum.getChildCount(); i++) {
-            groupRum.getChildAt(i).setEnabled(state);
-        }
-        for (int i = 0; i < groupMary.getChildCount(); i++) {
-            groupMary.getChildAt(i).setEnabled(state);
-        }
-        for (int i = 0; i < groupWine.getChildCount(); i++) {
-            groupWine.getChildAt(i).setEnabled(state);
+
+        for (LinearLayout grp : groups) {
+            for (int i = 0; i < grp.getChildCount(); i++) {
+                grp.getChildAt(i).setEnabled(state);
+            }
         }
 
     }
@@ -143,31 +135,13 @@ public class MainActivity extends AppCompatActivity {
         String correctAnswer = "";
         int location[]=new int[2];
 
+        for (Button btn : answerButtons) {
+            if (view.getId() == btn.getId()) {
+                correctAnswer = (String) btn.getTag();
+            }
+        }
+
         view.getLocationOnScreen(location);
-//        Log.v("MainActivity", String.valueOf(view.getId()));
-
-        if (view.getId() == bondAnswerButton.getId()) {
-            correctAnswer = "vodka and shaken";
-        }
-        if (view.getId() == rumAnswerButton.getId()) {
-            correctAnswer = "sugarcane";
-        }
-        if (view.getId() == ginAnswerButton.getId()) {
-            correctAnswer = "gin";
-        }
-        if (view.getId() == whiskyAnswerButton.getId()) {
-            correctAnswer = "all countries";
-        }
-        if (view.getId() == piscoAnswerButton.getId()) {
-            correctAnswer = "Chile and Peru";
-        }
-        if (view.getId() == maryAnswerButton.getId()) {
-            correctAnswer = "tomato";
-        }
-        if (view.getId() == wineAnswerButton.getId()) {
-            correctAnswer = "Vatican City";
-        }
-
         Toast toast = Toast.makeText(this, correctAnswer, Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.TOP|Gravity.START, location[0], location[1]+90);
         toast.show();
@@ -175,6 +149,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void reset(View view) {
         setContentView(R.layout.activity_main);
-        resetButton.setVisibility(View.INVISIBLE);
     }
 }
