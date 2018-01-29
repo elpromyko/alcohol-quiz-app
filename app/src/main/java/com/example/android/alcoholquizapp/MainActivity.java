13b27c4a3ffe4be6a580b106764815792b716f81
Piotr Prominski <piotr.prominski@gmail.com>
2018-01-29 14:39:27 +0100

    fix onRestoreInstanceState, edit strings, move variables initialization to onCreate(), correct question texts, add comments

--- app/src/main/java/com/example/android/alcoholquizapp/MainActivity.java
+++ app/src/main/java/com/example/android/alcoholquizapp/MainActivity.java
@@ -30,75 +30,30 @@ public class MainActivity extends AppCompatActivity {
     List<LinearLayout> groups;
     List<RadioButton> radioCorrectAnswers;
     TextView finalScore;
-    static final String WHISKY_STATE = "whiskyQuestion";
     static final String WHISKY_GROUP_STATE = "whiskyGroup";
+    static final String SCORE = "score";
+    static final String SCORE_VISIBILITY = "scoreVisibility";
+    static final String ANSWER_VISIBILITY = "answerVisibility";
+    static final String RESET_VISIBILITY = "resetVisibility";
+    static final String SUBMIT_STATE = "submitButtonState";
+    static final String USER_ANSWER_STATE = "userTypedAnswerState";
+    static final String USER_ANSWER_FOCUSABLE = "userAnswerFocusable";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        finalScore = findViewById(R.id.final_score);
+        userTypedAnswer = findViewById(R.id.spirit_name);
+
+        // groups of answers
         groupWhisky = findViewById(R.id.group_whisky);
         groupRum = findViewById(R.id.group_rum);
         groupBond = findViewById(R.id.group_bond);
         groupPisco = findViewById(R.id.group_pisco);
         groupMary = findViewById(R.id.group_mary);
         groupWine = findViewById(R.id.group_wine);
-        groups = Arrays.asList(groupWhisky, groupRum, groupMary, groupWine, groupBond, groupPisco);
-
-
-
-    }
-// ===WORK IN PROGRESS===
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        // Save the user's current score state for both players
-//        groupWhisky.getChildAt(0).isEnabled();
-
-        savedInstanceState.putInt(WHISKY_STATE, groupWhisky.getCheckedRadioButtonId());
-        savedInstanceState.putBoolean(WHISKY_GROUP_STATE, groupWhisky.getChildAt(0).isEnabled());
-//        savedInstanceState.putBoolean("MyBoolean", true);
-//        savedInstanceState.putInt(PLAYER2_SCORE, scorePlayer2);
-
-        savedInstanceState.putInt("Score", Integer.parseInt(finalScore.getText().toString()));
-        savedInstanceState.putInt(finalScore.getVisibility()));
-
-
-        // Always call the superclass so it can save the view hierarchy state
-        super.onSaveInstanceState(savedInstanceState);
-    }
-
-    public void onRestoreInstanceState(Bundle savedInstanceState) {
-        // Always call the superclass so it can restore the view hierarchy
-        super.onRestoreInstanceState(savedInstanceState);
-
-
-        // Restore score states from saved instance and display them
-        groupWhisky.check(savedInstanceState.getInt(WHISKY_STATE));
-
-        for (LinearLayout group : groups) {
-            for (int i = 0; i < group.getChildCount(); i++) {
-                group.getChildAt(i).setEnabled(savedInstanceState.getBoolean(WHISKY_GROUP_STATE));
-            }
-        }
-
-
-
-//        savedInstanceState.getBoolean(WHISKY_GROUP_STATE);
-
-//        View butt = findViewById(groupWhisky.getCheckedRadioButtonId());
-//        butt.setEnabled(false);
-
-//        scorePlayer1 = savedInstanceState.getInt(PLAYER1_SCORE);
-//        scorePlayer2 = savedInstanceState.getInt(PLAYER2_SCORE);
-//        displayPlayer1Score(scorePlayer1);
-//        displayPlayer2Score(scorePlayer2);
-    }
-// =====END WIP=======
-
-    public void submitAnswers(View view) {
-
-        finalScore = findViewById(R.id.final_score);
 
         // buttons
         submitButton = findViewById(R.id.submit_button);
@@ -121,32 +76,61 @@ public class MainActivity extends AppCompatActivity {
         answerChile = findViewById(R.id.check_chile);
         answerPeru = findViewById(R.id.check_peru);
 
-        // radio
+        // correct radio
         answerAllCountries = findViewById(R.id.radio_all_countries);
         answerSugarcane = findViewById(R.id.radio_cane);
         answerTomato = findViewById(R.id.radio_tomato);
         answerVatican = findViewById(R.id.radio_vatican);
 
-        userTypedAnswer = findViewById(R.id.spirit_name);
-
-//        groupWhisky = findViewById(R.id.group_whisky);
-//        groupBond = findViewById(R.id.group_bond);
-//        groupPisco = findViewById(R.id.group_pisco);
-//        groupRum = findViewById(R.id.group_rum);
-//        groupMary = findViewById(R.id.group_mary);
-//        groupWine = findViewById(R.id.group_wine);
-
+        // lists
         answerButtons = Arrays.asList(bondAnswerButton, rumAnswerButton, ginAnswerButton,
                 whiskyAnswerButton, piscoAnswerButton, maryAnswerButton, wineAnswerButton);
-
-//        groups = Arrays.asList(groupWhisky, groupRum, groupMary, groupWine, groupBond, groupPisco);
-
+        groups = Arrays.asList(groupWhisky, groupRum, groupMary, groupWine, groupBond, groupPisco);
         radioCorrectAnswers = Arrays.asList(answerSugarcane, answerAllCountries, answerTomato, answerVatican);
 
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save current states and visibilities for buttons and answers
+        savedInstanceState.putBoolean(WHISKY_GROUP_STATE, groupWhisky.getChildAt(0).isEnabled());
+        savedInstanceState.putCharSequence(SCORE, finalScore.getText());
+        savedInstanceState.putInt(SCORE_VISIBILITY, finalScore.getVisibility());
+        savedInstanceState.putInt(ANSWER_VISIBILITY, bondAnswerButton.getVisibility());
+        savedInstanceState.putInt(RESET_VISIBILITY, resetButton.getVisibility());
+        savedInstanceState.putBoolean(SUBMIT_STATE, submitButton.isEnabled());
+        savedInstanceState.putBoolean(USER_ANSWER_STATE, userTypedAnswer.isEnabled());
+        savedInstanceState.putBoolean(USER_ANSWER_FOCUSABLE, userTypedAnswer.isFocusable());
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore state (enabled / disabled) for all group of answers
+        for (LinearLayout group : groups) {
+            for (int i = 0; i < group.getChildCount(); i++) {
+                group.getChildAt(i).setEnabled(savedInstanceState.getBoolean(WHISKY_GROUP_STATE));
+            }
+        }
+        finalScore.setText(savedInstanceState.getCharSequence(SCORE));
+        finalScore.setVisibility(savedInstanceState.getInt(SCORE_VISIBILITY));
+
+        // Restore visibility for answer , reset and submit buttons
+        for (Button btn : answerButtons) {
+            btn.setVisibility(savedInstanceState.getInt(ANSWER_VISIBILITY));
+        }
+        resetButton.setVisibility(savedInstanceState.getInt(RESET_VISIBILITY));
+        submitButton.setEnabled(savedInstanceState.getBoolean(SUBMIT_STATE));
+
+        // Restore EditText View state
+        userTypedAnswer.setEnabled(savedInstanceState.getBoolean(USER_ANSWER_STATE));
+        userTypedAnswer.setFocusable(savedInstanceState.getBoolean(USER_ANSWER_FOCUSABLE));
+    }
+
+    public void submitAnswers(View view) {
         changeButtonState(false);
-
         displayFinalScore(calculateScore());
-
     }
 
     public int calculateScore() {
@@ -170,7 +154,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayFinalScore(int score) {
-        finalScore.setText("SCORE: " + score + "/7");
+        finalScore.setText(String.format("SCORE: %d/7", score));
+        finalScore.setVisibility(View.VISIBLE);
     }
 
     public void changeButtonState(boolean state) {
@@ -191,6 +176,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // Shows toast message below button with button tag dependant text
     public void viewAnswer(View view) {
         String correctAnswer = "";
         int location[]=new int[2];
